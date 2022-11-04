c74ff8632323bfd64dd085e85a0704962ca0989f
crackheadakira <lasn.mine@gmail.com>
2022-11-04 21:41:11 +0100

    Initialize repository

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import Album from './components/Album.vue'
+</script>
+
+<template>
+  <Album artistName="Joji" />
+</template>
+
+<style scoped>
+body {
+  width: 100%;
+  height: 100%;
+}
+</style>
